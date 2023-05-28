06d5686c3809b9e3451b7957176f4814e8d5a8a3
jameshuang98 <jameshuang337@gmail.com>
2023-05-28 13:28:35 -0700

    implement onRefresh

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -17,7 +17,11 @@ const JobDetails = () => {
     const [refreshing, setRefreshing] = useState(false);
     const [activeTab, setActiveTab] = useState(tabs[0])
 
-    const onRefresh = () => { };
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        refetch();
+        setRefreshing(false);
+    }, []);
 
     const displayTabContent = () => {
         switch (activeTab) {
@@ -94,7 +98,7 @@ const JobDetails = () => {
                     )}
                 </ScrollView>
 
-                <JobFooter url={data[0]?.job_google.link ?? 'https://careers.google.com/jobs/results'}/>
+                <JobFooter url={data[0]?.job_google.link ?? 'https://careers.google.com/jobs/results'} />
             </>
         </SafeAreaView>
     )
