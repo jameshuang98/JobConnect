a1e7cd03fa071e471e54736c6eef1fb0a0bbc642
jameshuang98 <jameshuang337@gmail.com>
2023-06-22 20:23:53 -0700

    fix issues

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text } from "react-native";
 
-import styles from './about.style'
+import styles from "./about.style";
 
 const About = ({ info }) => {
   return (
-    <View style={style.container}>
+    <View style={styles.container}>
       <Text style={styles.headText}>About the job:</Text>
 
       <View style={styles.contentBox}>
         <Text style={styles.contextText}>{info}</Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default About
+export default About;
